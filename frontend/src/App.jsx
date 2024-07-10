3e755427bdea1dbe0be9cdcf3cc0c9f4a745fd55
josrph135 <oluwafemijosrph@gmail.com>
2024-07-10 23:16:56 +0100

    reload error fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import RApp from "./component/app";
 import "./App.css";
-function App() {
-  return (
-    <div>
-      <RApp />
-    </div>
-  );
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home";
+import Delivery from "./delivery";
+import Updateform from "./updateform";
 
-export default App;
+const RiderApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home />}></Route>
+        <Route path="/delivery" element={<Delivery />}></Route>
+        <Route path="/updateform/:id" element={<Updateform />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RiderApp;
